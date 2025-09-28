09645fc151be24e76d3bcb723dcc7118c4722f53
Arin0303 <cindy030225@gmail.com>
2025-09-28 19:01:42 +0900

    fix: gitignore수정 및 git 추적 캐시 제거

--- class_map/recommendation_lecture/chatbot/gpt_client.py
+++
@@ -1,9 +0,0 @@
-import os
-from openai import AzureOpenAI
-
-def create_azure_client():
-    return AzureOpenAI(
-        api_key=os.getenv("AZURE_OPENAI_KEY"),
-        azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
-        api_version=os.getenv("AZURE_OPENAI_API_VERSION")
-    )
